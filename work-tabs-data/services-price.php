de3ee4e8667adb20dddc54a6777331b514faa600
Danil <DukhovnikovDanil@yandex.ru>
2020-05-08 18:56:18 +0300

    tab is work

---
+++ work-tabs-data/services-price.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+  <link rel="shortcut icon" href="assets/img/helpers/favicon.png" type="image/png" />
+  <link rel="stylesheet" href="assets/css/libs.min.css" />
+  <link rel="stylesheet" href="assets/css/main.css?v=1.0.0" />
+  <link rel="stylesheet" href="assets/css/responsive.css?v=1.0.0" />
+  <link rel="stylesheet" href="assets/css/header.css?v=1.0.0" />
+  <link rel="stylesheet" href="assets/css/footer.css?v=1.0.0" />
+  <link rel="stylesheet" href="assets/css/services-price.css?v=1.0.0" />
+
+  <title>О клинике</title>
+</head>
+
+<body>
+
+  <div class="version">1.0.0</div>
+
+  <section class="services-prices">
+    <div class="container">
+      <ul class="tabs">
+        <li><a href="#tab1"></a>Сведения</li>
+        <li><a href="#tab2"></a>Цены</li>
+      </ul>
+      <div class="tab-container">
+        <div id="tab1" class="tab-container__content">
+          <div class="general-info-information">
+            <div class="row">
+              <div class="col-lg-8 col-md-6">
+                <div class="general-info-information__advise">
+                  <p>
+                    Регулярные осмотры у ветеринарных терапевтов — залог
+                    благополучной жизни животных.
+                  </p>
+                </div>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                <div class="general-info-buttons">
+                  <button class="general-info-buttons__record">
+                    <p>Записаться на прием</p>
+                  </button>
+                  <button class="general-info-buttons__view">
+                    <p>Смотреть истории пациентов</p>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="general-info-text">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="general-info-text-left">
+                  <p>
+                    Если обнаружены явные проблемы в организме, или в целом вам
+                    важно поддержание здоровья питомца, обращение к терапевту
+                    является важным и нужным. Своевременная диагностика
+                    позволяет поддерживать состояние здоровья домашнего
+                    животного в хорошем состоянии и выявлять потенциальные
+                    проблемы, прежде чем они станут серьезными. Чем регулярнее
+                    будут визиты к врачу, тем в большей безопасности будет
+                    находиться ваш питомец, он привыкнет к осмотрам
+                    специалистов, они перестанут вызывать у него стресс, и это
+                    облегчит будущие визиты в клинику.</br></br>
+                  </p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="general-info-text-right">
+                  <p>
+                    В ветеринарном центре КорВетЦентр на приеме у ветеринара
+                    домашний питомец будет тщательно обследован грамотным
+                    специалистом, и вы получите исчерпывающую информацию о
+                    состоянии его здоровья. Терапевтический осмотр предполагает
+                    общий осмотр животного (если имеются показания — назначаются
+                    дополнительные исследования), постановку диагноза в случае
+                    имеющегося заболевания, назначение лечения, рекомендации по
+                    уходу, содержанию и профилактике. При этом цена прима у
+                    ветеринара вполне доступна и демократична.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="tab2" class="tab-container__content">
+          <div class="prices-wrapper">
+            <div class="row">
+              <div class="col-xl-8 col-md-12">
+                <div class="prices-list-wrapper">
+                  <ul class="prices-list">
+                    <li><span>Взятие крови</span><b>200 р.</b></li>
+                    <li><span>Капельное внутривенное введение растворов 1 час</span><b>150 р.</b></li>
+                    <li><span>Инъекция в/в, в/м, п/к</span><b>20 р.</b></li>
+                    <li><span>Оральное введение препаратов</span><b>150 р.</b></li>
+                    <li><span>Катетеризация мочевого пузыря коты, кобели</span><b>350 р.</b></li>
+                    <li><span>Постановка венозного периферического катетера</span><b>200 р.</b></li>
+                    <li><span>Клизма очистительная</span><b>1000 р.</b></li>
+                    <li><span>Эпибульбарная инстилляция в течении суток и еще какие-то слова, чтобы текст был в
+                        несколько
+                        строк</span><b>35 000 р.</b></li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-xl-4 col-md-12">
+                <div class="prices-buttons-wrapper">
+                  <button class="general-info-buttons__record">
+                    <p>Записаться на прием</p>
+                  </button>
+                  <button class="general-info-buttons__view">
+                    <p>Смотреть истории пациентов</p>
+                  </button>
+                  <div class="prices-buttons-wrapper__actual-date-info">
+                  </div>
+                </div>
+                <div class="price-calendar-wrapper">
+                  <img src="assets/img/services-price/price-calendar.svg" alt="calendar">
+                  <div class="price-calendar-wrapper__text">
+                    <p>Цены от</p>
+                    <b>26 апреля 2020 года</b>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+  <script src="assets/js/libs.min.js"></script>
+  <script src="assets/js/share.js?v=1.0.0"></script>
+  <script src="assets/js/main.js?v=1.0.0"></script>
+  <script src="assets/js/services-price.js?v=1.0.0"></script>
+</body>
+
+</html>
